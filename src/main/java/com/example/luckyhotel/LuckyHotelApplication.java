4eda35dc414bd21a2a1fe18d706c0eb189ccfbbd
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-21 20:46:04 +0300

    Initial commit

---
+++ src/main/java/com/example/luckyhotel/LuckyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.luckyhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyHotelApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/luckyhotel/model/Review.java
@@ -0,0 +1,2 @@
+package com.example.luckyhotel.model;public class Review {
+}
